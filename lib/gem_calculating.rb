56538d4727e3710fa61ad7b71163746f99be7742
OstafiichukO <issimo333@gmail.com>
2022-10-21 14:14:54 +0300

    perf: module update

--- lib/gem_calculating.rb
+++ lib/gem_calculating.rb
@@ -1,44 +1,49 @@
 # frozen_string_literal: true
-
 require_relative "gem_calculating/version"
 
-# Calculator gem
-module GemCalculating
-  # Class for calculating
-  class Calculator
-    # Adds two numbers
-    def self.add(num1, num2)
-      if validate_numbers(num1, num2) == true
-        return puts "Numbers is not valid"
-      end
-      num1 + num2
-    rescue StandardError
-      nil
+module Calculate
+  # Adds two numbers
+  def self.add(num1, num2)
+    if validate_numbers(num1, num2) == true
+      return puts "Numbers is not valid"
     end
-    # Subtract two numbers
-    def self.subtract(num1, num2)
-      num1 - num2
-    rescue StandardError
+    num1 + num2
+  rescue StandardError
     nil
+  end
+  # Subtract two numbers
+  def self.subtract(num1, num2)
+    if validate_numbers(num1, num2) == true
+      return puts "Numbers is not valid"
     end
-    # Multiply two numbers
-    def self.multiply(num1, num2)
-      num1 * num2
-    rescue StandardError
-      nil
+    num1 - num2
+  rescue StandardError
+  nil
+  end
+  # Multiply two numbers
+  def self.multiply(num1, num2)
+    if validate_numbers(num1, num2) == true
+      return puts "Numbers is not valid"
     end
-    # Divide two numbers
-    def self.divide(num1, num2)
-      num1 / num2
-    rescue StandardError
-      nil
+    num1 * num2
+  rescue StandardError
+    nil
+  end
+  # Divide two numbers
+  def self.divide(num1, num2)
+    if validate_numbers(num1, num2) == true
+      return puts "Numbers is not valid"
     end
-
-    private_class_method def self.validate_numbers(num1, num2)
-      num1.is_a?(String) or num2.is_a?(String)
-    end
+    num1 / num2
+  rescue StandardError
+    nil
   end
 
-  # class Error < StandardError; end
-  # Your code goes here...
+  private_class_method def self.validate_numbers(num1, num2)
+    num1.is_a?(String) or num2.is_a?(String)
+end
+
+
+class Error < StandardError; end
+# Your code goes here...
 end
--- spec/gem_calculating_spec.rb
+++ spec/gem_calculating_spec.rb
@@ -1,39 +1,39 @@
 require 'gem_calculating'
 
-describe GemCalculating::Calculator do
+describe Calculate do
   it 'can add two numbers' do
-    expect(GemCalculating::Calculator.add(2, 3)).to eq(5)
+    expect(Calculate.add(2, 3)).to eq(5)
   end
 
   it 'cannot add strings' do
-    expect(GemCalculating::Calculator.add('2', '3')).to eq(nil)
+    expect(Calculate.add('2', '3')).to eq(nil)
   end
 
   it 'can subtract two numbers' do
-    expect(GemCalculating::Calculator.subtract(3, 2)).to eq(1)
+    expect(Calculate.subtract(3, 2)).to eq(1)
   end
 
   it 'cannot subtract strings' do
-    expect(GemCalculating::Calculator.subtract('3', '2')).to eq(nil)
+    expect(Calculate.subtract('3', '2')).to eq(nil)
   end
 
   it 'can multiply two numbers' do
-    expect(GemCalculating::Calculator.multiply(4, 2)).to eq(8)
+    expect(Calculate.multiply(4, 2)).to eq(8)
   end
 
   it 'cannot multiply strings' do
-    expect(GemCalculating::Calculator.multiply('4', '2')).to eq(nil)
+    expect(Calculate.multiply('4', '2')).to eq(nil)
   end
 
   it 'can divide two numbers' do
-    expect(GemCalculating::Calculator.divide(4, 2)).to eq(2)
+    expect(Calculate.divide(4, 2)).to eq(2)
   end
 
   it 'cannot divide by zero' do
-    expect(GemCalculating::Calculator.divide(4, 0)).to eq(nil)
+    expect(Calculate.divide(4, 0)).to eq(nil)
   end
 
   it 'cannot divide strings' do
-      expect(GemCalculating::Calculator.divide('4', '2')).to eq(nil)
+      expect(Calculate.divide('4', '2')).to eq(nil)
   end
 end
